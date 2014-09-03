13216ede3c3008a1bed1ad2d02cdbe1d05374c46
ttacon <ttacon@gmail.com>
2014-09-03 17:57:25 -0400

    ZCOUNT

--- sortedsets.go
+++ sortedsets.go
@@ -16,9 +16,12 @@ func (c *Client) ZCard(key string) (int64, error) {
 	return c.intResp(resp)
 }
 
-func (c *Client) ZCount(key, min, max string) (int, error) {
-	// TODO(ttacon): do it
-	return 0, nil
+func (c *Client) ZCount(key, min, max string) (int64, error) {
+	resp, err := c.exec("ZCOUNT", key, min, max)
+	if err != nil {
+		return 0, err
+	}
+	return c.intResp(resp)
 }
 
 func (c *Client) ZIncryBy(key, increment, member string) (string, error) {
